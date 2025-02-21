95e1cce90ffda87042c48753385c4f1d7bd4f2cc
AstroWalker24 <nadeemshaik2003@gmail.com>
2025-02-21 09:38:23 +0530

    feat: configure app with settings and initialize ORM

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from .src.config import config 
+from .src.database import db
 
 
 app = Flask(__name__)
 
+# configuring the app with the configuration settings from the config module 
+app.config.from_object(config)
 
+# initializing the ORM with the app
+db.init_app(app)
 
 
 
