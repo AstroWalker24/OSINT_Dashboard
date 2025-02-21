66d70b8731496824d1f51555c6041998f10e5889
AstroWalker24 <nadeemshaik2003@gmail.com>
2025-02-21 16:15:44 +0530

    feat: add OAuth config and update db connection env variables

--- backend/src/config/config.py
+++ backend/src/config/config.py
@@ -8,25 +8,29 @@ load_dotenv()
 # base configuration class 
 class BaseConfig:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    OAUTH_REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI")
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # development phase config class extending the base config class 
 class DevConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DB_CONNECTION_STRING","sqlite:///dev.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_CONNECTION_STRING_DEV","sqlite:///dev.db")
 
 
 # production phase config class extending the base config class 
 class ProdConfig(BaseConfig):
     DEBUG = False 
-    SQLALCHEMY_DATABASE_URI = os.getenv("PROD_DB_URL","sqlite:///prod.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_CONNECTION_STRING_PROD","sqlite:///prod.db")
 
 
 # testing phase config class extending the base config class 
 class TestConfig(BaseConfig):
     TESTING = True 
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DB_URL","sqlite:///test.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_CONNECTION_STRING_TEST","sqlite:///test.db")
 
 
 
